d968eb903d1e9b3a7dab2c3570852f79ca053317
mrmalvicino <mrmalvicino@gmail.com>
2023-05-12 17:56:50 -0300

    tp5-ej6

--- tp5-ej6.cpp
+++ tp5-ej6.cpp
@@ -20,5 +20,62 @@ Calcular e informar:
 #include <iostream>
 
 int main(){
+    int prod;                   // Código de producto *
+    int type;                   // Tipo *
+    float quant;                // Cantidad vendida *
+    float value;                // Total recaudado *
+    float min_val;              // Recaudación mínima
+    int min_prod;               // Producto que recaudó el mínimo
+    bool flag_min = 0;          // Se registró el primer mínimo?
+    float accu_1 = 0;           // Recaudado por facturas
+    float accu_2 = 0;           // Recaudado por tortas
+    float accu_3 = 0;           // Recaudado por pan
+    float accu_tot = 0;         // Recaudación total
 
+    std::cout << "Código de producto:" << std::endl;
+    std::cin >> prod;
+
+    while(0 <= prod){
+        std::cout << "Tipo de producto:" << std::endl;
+        std::cin >> type;
+
+        std::cout << "Cantidad vendida:" << std::endl;
+        std::cin >> quant;
+
+        std::cout << "Total recaudado:" << std::endl;
+        std::cin >> value;
+
+        if(flag_min == 1){
+            if(value < min_val){
+                min_val = value;
+                min_prod = prod;
+            }
+        } else{
+            min_val = value;
+            min_prod = prod;
+            flag_min = 1;
+        }
+
+        accu_tot = accu_tot + value;
+
+        switch(type){
+        case 1:
+            accu_1 = accu_1 + value;
+            break;
+        case 2:
+            accu_2  = accu_2 + value;
+            break;
+        case 3:
+            accu_3  = accu_3 + value;
+            break;
+        }
+
+        std::cout << "Código de producto:" << std::endl;
+        std::cin >> prod;
+    }
+
+    std::cout << "Producto que recaudó el mínimo: cod. " << min_prod << std::endl;
+    std::cout << "Porcentaje de recaudación por facturas: " << (accu_1 / static_cast<float>(accu_tot)) * 100 << " %" << std::endl;
+    std::cout << "Porcentaje de recaudación por tortas: " << (accu_2 / static_cast<float>(accu_tot)) * 100 << " %" << std::endl;
+    std::cout << "Porcentaje de recaudación por pan: " << (accu_3 / static_cast<float>(accu_tot)) * 100 << " %" << std::endl;
 }
