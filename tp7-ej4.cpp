d8a3a447e81d5e7d2ae272a080ca38a5560915ac
mrmalvicino <mrmalvicino@gmail.com>
2023-06-01 01:12:30 -0300

    tp7-ej4

--- tp7-ej4.cpp
+++ tp7-ej4.cpp
@@ -5,6 +5,21 @@ Hacer una función que reciba un vector de números enteros y su tamaño y cargu
 
 #include <iostream>
 
+void load_array(int arr[], int size){
+    for (int i = 0; i < size; i++){
+        std::cout << "Ingresar entero:" << std::endl;
+        std::cin >> arr[i];
+    }
+}
+
 int main(){
+    int const SIZE = 3;
+    int arr[SIZE];
+    load_array(arr, SIZE);
     
+    std::cout << "Los enteros ingresados fueron:" << std::endl;
+    
+    for (int i = 0; i < SIZE; i++){
+        std::cout << arr[i] << std::endl;
+    }
 }
