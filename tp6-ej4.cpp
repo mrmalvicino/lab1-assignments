c4426dce582ac578d2d864edf6d3487d0aab485c
mrmalvicino <mrmalvicino@gmail.com>
2023-06-01 00:28:33 -0300

    tp6-ej4

--- tp6-ej4.cpp
+++ tp6-ej4.cpp
@@ -9,7 +9,42 @@ El número de día que más se recaudó en total y cuál fue la recaudación má
 */
 
 #include <iostream>
+using namespace std;
 
 int main(){
-    
+    int sale_number;                        // Número de venta
+    int day;                                // Número de día
+    float amount;                           // Importe de la venta
+    int const DAYS_IN_MONTH = 3;           // Cantidad de días del mes pasado
+    float sales_vec[DAYS_IN_MONTH] = {};    // Vector de recaudaciones de cada día del mes
+    float max = 0;                          // Máxima recaudación de un día
+    int max_day;                            // Día de máxima recaudación
+
+    cout << "Número de venta:" << endl;
+    cin >> sale_number;
+
+    while(sale_number != 0){
+        cout << "Número de día:" << endl;
+        cin >> day;
+
+        cout << "Importe de la venta:" << endl;
+        cin >> amount;
+
+        sales_vec[day - 1] = sales_vec[day - 1] + amount;
+
+        cout << "Número de venta:" << endl;
+        cin >> sale_number;
+    }
+
+    for(int i = 0; i < DAYS_IN_MONTH; i++){
+        if(sales_vec[i] != 0){
+            cout << "Recaudación del día " << i + 1 << " : " << sales_vec[i] << endl;
+        }
+        if(max < sales_vec[i]){
+            max = sales_vec[i];
+            max_day = i + 1;
+        }
+    }
+
+    cout << "El día " << max_day << " se recaudó el máximo: $" << max << endl;
 }
