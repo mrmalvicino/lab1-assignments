106cee8a1b7597665f17d29113dd5d95c6f83c59
mrmalvicino <mrmalvicino@gmail.com>
2023-06-01 23:31:33 -0300

    tp6-ej5, update .json

--- tp6-ej5.cpp
+++ tp6-ej5.cpp
@@ -8,5 +8,22 @@ Sólo deben utilizarse dos vectores en el programa: pos y nega.
 #include <iostream>
 
 int main(){
-    
+    int const N = 10;
+    float pos[N] = {};
+    float neg[N] = {};
+    float var;
+
+    for(int i = 0; i < N; i++){
+        std::cin >> var;
+        if(0 < var){
+            pos[i] = var;
+        } else if(var < 0){
+            neg[i] = var;
+        }
+    }
+
+    for(int i = 0; i < N; i++){
+        std::cout << "pos[" << i << "] = " << pos[i] << " ; ";
+        std::cout << "neg[" << i << "] = " << neg[i] << " ; ";
+    }
 }
