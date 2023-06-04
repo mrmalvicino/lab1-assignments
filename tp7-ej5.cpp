e4b322f47a93cdf437badb9062225869268d873c
mrmalvicino <mrmalvicino@gmail.com>
2023-06-04 18:35:25 -0300

    tp7-ej5

--- tp7-ej5.cpp
+++ tp7-ej5.cpp
@@ -5,6 +5,9 @@ Hacer una función que reciba un vector de números enteros y su tamaño y liste
 
 #include <iostream>
 
-int main(){
-    
+void printArray(int arr[], int const SIZE){
+    for (int i = 0; i < SIZE - 1; i++){
+        std::cout << arr[i] << " ";
+    }
+    std::cout << arr[SIZE - 1];
 }
