e602f6cd525c3eab497e71d37f3fc985ef4eb7d3
mrmalvicino <mrmalvicino@gmail.com>
2023-06-04 19:22:04 -0300

    tp7-ej6

--- tp7-ej6.cpp
+++ tp7-ej6.cpp
@@ -6,6 +6,27 @@ Caso contrario debe devolver falso.
 
 #include <iostream>
 
-int main(){
+bool val_in_arr(int val, int arr[], int const ARR_SIZE){
+    bool flag = 0;
     
+    for(int i = 0; i < ARR_SIZE; i++){
+        if(arr[i] == val){
+            flag = true;
+        }
+    }
+
+    return flag;
 }
+
+int main(){
+    int val_1 = 3;
+    int val_2 = 9;
+    int arr[5] = {1, 2, 3, 4, 5};
+    std::cout << val_in_arr(val_1, arr, 5) << std::endl; // Verdadero
+    std::cout << val_in_arr(val_2, arr, 5) << std::endl; // Faso
+}
+
+/*
+Compile and run in terminal:
+./bin/tp7-ej6
+*/
